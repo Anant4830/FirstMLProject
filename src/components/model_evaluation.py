cb40ded19c8d03e2e83e78f7d60011cd88b1b921
Anant Mohan <anant4830@gmail.com>
2024-11-14 14:42:02 +0530

    mlflow added

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,7 +1,5 @@
 import os
 import sys
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from urllib.parse import urlparse
 
 import mlflow
 import mlflow.sklearn
@@ -9,21 +7,53 @@ import numpy as np
 import pickle
 
 from src.utils.utils import load_object
+from src.exceptions.exception import customException
+from src.logger.custom_logging import logging
 
-from dataclasses import dataclass
-from pathlib import Path
+from urllib.parse import urlparse
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-@dataclass
-class ModelEvaluationConfig:
-    pass
 
 class ModelEvaluation:
     def __init__(self):
-        pass
+        logging.info("Evaluation started...!!")
 
-    def initiate_model_evaluation(self):
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        logging.info("all evaluation metrices are captured....!!")
+        return rmse, mae, r2
+    
+    def initiate_model_evaluation(self, train_array, test_array):
         try:
-            pass
+            X_test, y_test = (test_array[:, :-1], test_array[:, -1])
+
+            model_path = os.path.join("artifacts", "model.pkl")
+            model = load_object(model_path)
+
+            #mlflow.set_registry_uri("")
+
+            logging.info("madel has been registered-----")
+
+            tracking_uri_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            print(tracking_uri_type_store)
+
+
+            with mlflow.start_run():
+                prediction = model.predict(X_test)
+                (rmse, mae, r2) = self.eval_metrics(y_test, prediction)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("mae", mae)
+                mlflow.log_metric("r2", r2)
+
+                if tracking_uri_type_store != "file":
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+                else:
+                    mlflow.sklearn.log_model(model, "model")
+
         except Exception as e:
-            logging.info()
+            logging.info("Evaluation complete...!!")
             raise customException(e, sys)
