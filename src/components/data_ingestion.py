2644966011b0165b6b37af6bc96ea8211ff4c55d
Sreya Goswami <getsreya@gmail.com>
2024-11-28 15:09:59 +0530

    Shared artifact storage implemented and revised directory deletion code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,32 +22,28 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Data Ingestion Started...!")
+        logging.info("Data Ingestion Started")
         try:
-            # data = pd.read_csv(r"C:\Users\Admin\Downloads\AI-839\MLOps\projects2\FirstMLProject\data\cubic_zirconia.csv")
-            #path_csv = os.path.abspath(os.path.join("..","..","data","cubic_zirconia.csv"))
-            # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-            # file_path = os.path.join(BASE_DIR, "data", "cubic_zirconia.csv")
-            # # data = pd.read_csv(path_csv)
             data = pd.read_csv(r"/app/data/cubic_zirconia.csv")
-            #data = pd.read_csv(file_path)
-            logging.info("Reading my dataFrame")
+            logging.info("Read Data")
 
-            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)))
-                        # os.path.dirname(os.path.join(self.ingestion_config.train_data_path)),
-                        # os.path.dirname(os.path.join(self.ingestion_config.test_data_path)))
+            artifacts_dir = "/app/artifacts"
+
+            if os.path.exists(artifacts_dir) and os.path.isdir(artifacts_dir):
+                for item in os.listdir(artifacts_dir):
+                    item_path = os.path.join(artifacts_dir, item)
+                    os.unlink(item_path)
             
             data.to_csv(self.ingestion_config.raw_data_path, index=False)
-            logging.info("I have saved the raw dataset in the artifacts folder")
+            logging.info("Saved Data to artifacts")
 
-            logging.info("Here, I have performed train test split")
+            logging.info("Train Test Split")
             train_data, test_data = train_test_split(data, test_size=0.25)
-            logging.info("train test split completed")
 
             train_data.to_csv(self.ingestion_config.train_data_path, index=False)
             test_data.to_csv(self.ingestion_config.test_data_path, index=False)
 
-            logging.info("data ingestion part completed")
+            logging.info("Data Ingestion Completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -55,7 +51,7 @@ class DataIngestion:
             )
    
         except Exception as e:
-            logging.info("Inside")
+            logging.info("Error in Data Ingestion")
             raise customException(e, sys)
         
 # if __name__ == "__main__":
